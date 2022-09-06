eb1ceb6c9232d7e74e5936abc34b33d6fdbb3fd6
Sajadqadir01 <sajadghadiri01@gmail.com>
2022-09-06 21:48:35 +0430

    add search system to project

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=50)
-    complete = models.BooleanField(default=False, null=True, blank=True)
+    complete = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
 
